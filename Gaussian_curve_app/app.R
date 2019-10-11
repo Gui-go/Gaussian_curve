59023f7b994fe878da01b71193640555be49b186
unknown <guilhermeviegas1993@gmail.com>
2019-10-11 12:33:44 -0300

    commiting...

--- Gaussian_curve_app/app.R
+++ Gaussian_curve_app/app.R
@@ -1,24 +1,40 @@
 library(shiny)
 ui <- fluidPage(
-    titlePanel("Probabilidade e Distribuição Normal"),
+    titlePanel("Distribuição de probabilidade Normal"),
     sidebarLayout(
         sidebarPanel(width = 2,
             numericInput(inputId = "mean_input", label = "Média", value = 0, step = 1),
             numericInput(inputId = "sd_input", label = "Desvio Padrão", value = 1, step = 1, min = 0.1),
-            selectInput(inputId = "tipo_input", label = "Tipo de modelo", choices = c("Entre", "Maior que", "Menor que"), selected = "Entre"),
-            conditionalPanel(
-                condition = "input.tipo_input == 'Entre'",
-                numericInput(inputId = "input_x1", label = "X1", value = -1, step = 1),
-                numericInput(inputId = "input_x2", label = "X2", value = 1, step = 1)
-            ),
-            conditionalPanel(
-                condition = "input.tipo_input == 'Maior que'",
-                numericInput(inputId = "input_x1_ma", label = "X1", value = -1, step = 1)
-            ),
-            conditionalPanel(
-                condition = "input.tipo_input == 'Menor que'",
-                numericInput(inputId = "input_x1_me", label = "X1", value = 1, step = 1)
-            )
+            selectInput(inputId = "v_p_input", label = "Tipo", choices = c("Por valores", "Por percentagem")),
+            conditionalPanel(condition = "input.v_p_input == 'Por valores'",
+                             selectInput(inputId = "tipo_input", label = "Seletor", choices = c("Entre", "Maior que", "Menor que"), selected = "Entre"),
+                             conditionalPanel(
+                                 condition = "input.tipo_input == 'Entre'",
+                                 numericInput(inputId = "input_x1", label = "a", value = -1, step = 1),
+                                 numericInput(inputId = "input_x2", label = "b", value = 1, step = 1)
+                             ),
+                             conditionalPanel(
+                                 condition = "input.tipo_input == 'Maior que'",
+                                 numericInput(inputId = "input_x1_ma", label = "a", value = -1, step = 1)
+                             ),
+                             conditionalPanel(
+                                 condition = "input.tipo_input == 'Menor que'",
+                                 numericInput(inputId = "input_x1_me", label = "a", value = 1, step = 1)
+                             )),
+            conditionalPanel(condition = "input.v_p_input == 'Por percentagem'",
+                             selectInput(inputId = "tipo_p_input", label = "Seletor", choices = c("Entre", "Maior que", "Menor que"), selected = "Entre"),
+                             conditionalPanel(
+                                 condition = "input.tipo_p_input == 'Entre'",
+                                 numericInput(inputId = "input_p_x1", label = "a", value = 0.5, min = 0, max = 1, step = 0.05)
+                             ),
+                             conditionalPanel(
+                                 condition = "input.tipo_p_input == 'Maior que'",
+                                 numericInput(inputId = "input_p_x1_ma", label = "a", value = 0.5, min = 0.5, step = 0.05)
+                             ),
+                             conditionalPanel(
+                                 condition = "input.tipo_p_input == 'Menor que'",
+                                 numericInput(inputId = "input_p_x1_me", label = "a", value = 0.5, min = 0, max = 1, step = 0.05)
+                             ))
         ),
         mainPanel(width = 10,
            plotOutput("Normal_plot"),
@@ -28,21 +44,52 @@ ui <- fluidPage(
 )
 server <- function(input, output) {
     library(tidyverse)
+    
+    autoInvalidate <- reactiveTimer(2000)
+    
     prob_reac <- reactive(
-        if(input$tipo_input == 'Entre'){(pnorm(q = input$input_x2, mean = input$mean_input, sd = input$sd_input) - pnorm(q = input$input_x1, mean = input$mean_input, sd = input$sd_input))} else
-        if(input$tipo_input == 'Maior que'){1-pnorm(q = input$input_x1_ma, mean = input$mean_input, sd = input$sd_input)} else
-        if(input$tipo_input == 'Menor que'){pnorm(q = input$input_x1_me, mean = input$mean_input, sd = input$sd_input)}    
+        if(input$v_p_input == 'Por valores' & input$tipo_input == 'Entre'){(pnorm(q = input$input_x2, mean = input$mean_input, sd = input$sd_input) - pnorm(q = input$input_x1, mean = input$mean_input, sd = input$sd_input))} else
+            if(input$v_p_input == 'Por valores' & input$tipo_input == 'Maior que'){1-pnorm(q = input$input_x1_ma, mean = input$mean_input, sd = input$sd_input)} else
+                if(input$v_p_input == 'Por valores' & input$tipo_input == 'Menor que'){pnorm(q = input$input_x1_me, mean = input$mean_input, sd = input$sd_input)} else
+                    if(input$v_p_input != 'Por valores' & input$tipo_input == 'Entre' & input$input_p_x1 == 1){(tigerstats::qnormGC(region = "between", area = 0.99999, mean = input$mean_input, sd = input$sd_input, graph = F))} else
+                        if(input$v_p_input != 'Por valores' & input$tipo_input == 'Maior que' & input$input_p_x1_ma == 1){(tigerstats::qnormGC(region = "above", area = 0.99999, mean = input$mean_input, sd = input$sd_input, graph = F))} else
+                            if(input$v_p_input != 'Por valores' & input$tipo_input == 'Menor que' & input$input_p_x1_me == 1){(tigerstats::qnormGC(region = "below", area = 0.99999, mean = input$mean_input, sd = input$sd_input, graph = F))} else
+                                if(input$v_p_input != 'Por valores' & input$tipo_p_input == 'Entre'){(tigerstats::qnormGC(region = "between", area = input$input_p_x1, mean = input$mean_input, sd = input$sd_input, graph = F))} else
+                                    if(input$v_p_input != 'Por valores' & input$tipo_p_input == 'Maior que'){(tigerstats::qnormGC(region = "above", area = input$input_p_x1_ma, mean = input$mean_input, sd = input$sd_input, graph = F))} else
+                                        if(input$v_p_input != 'Por valores' & input$tipo_p_input == 'Menor que'){(tigerstats::qnormGC(region = "below", area = input$input_p_x1_me, mean = input$mean_input, sd = input$sd_input, graph = F))}
     )
     output$Prob_func <- renderUI({
-        if(input$tipo_input == 'Entre'){h2(strong(withMathJax(paste0("$$P(", input$input_x1, " < X <", input$input_x2, ") = ", round(prob_reac(), 3), "$$"))))} else
-        if(input$tipo_input == 'Maior que'){h2(strong(withMathJax(paste0("$$P( X > ", input$input_x1_ma, ") = ", round(prob_reac(), 3), "$$"))))} else
-        if(input$tipo_input == 'Menor que'){h2(strong(withMathJax(paste0("$$P( X <", input$input_x1_me, ") = ", round(prob_reac(), 3), "$$"))))}
+        autoInvalidate()
+        if(input$v_p_input == 'Por valores'){
+            if(input$tipo_input == 'Entre'){tt <- h2(strong(withMathJax(paste0("$$P(", input$input_x1, " < X <", input$input_x2, ") = ", round(prob_reac(), 3), "$$"))))}
+            if(input$tipo_input == 'Maior que'){tt <- h2(strong(withMathJax(paste0("$$P( X > ", input$input_x1_ma, ") = ", round(prob_reac(), 3), "$$"))))}
+            if(input$tipo_input == 'Menor que'){tt <- h2(strong(withMathJax(paste0("$$P( X <", input$input_x1_me, ") = ", round(prob_reac(), 3), "$$"))))}
+        } else
+            if(input$v_p_input != 'Por valores'){
+                if(input$tipo_p_input == 'Entre'){
+                    if(input$tipo_p_input == 'Entre' & input$input_p_x1 == 1){tt <- h2(strong(withMathJax(paste0("$$P(", -Inf, " < X <", Inf, " ) = ", round(input$input_p_x1, 3), "$$"))))}
+                    if(input$input_p_x1 != 1){tt <- h2(strong(withMathJax(paste0("$$P(", round(prob_reac()[1], 3), " < X <", round(prob_reac()[2], 3), ") = ", round(input$input_p_x1, 3), "$$"))))}
+                }
+            } else
+                if(input$tipo_p_input == 'Maior que'){
+                    if(input$tipo_p_input == 'Maior que' & input$input_p_x1_ma == 1){tt <- h2(strong(withMathJax(paste0("$$P(", -Inf, " < X <", Inf, " ) = ", round(input$input_p_x1, 3), "$$"))))}
+                    if(input$input_p_x1_ma != 1){tt <- h2(strong(withMathJax(paste0("$$P( X >", round(prob_reac()[1], 3), ") = ", round(input$input_p_x1_ma, 3), "$$"))))}
+                } else
+                    if(input$tipo_p_input == 'Menor que'){
+                        if(input$tipo_p_input == 'Menor que' & input$input_p_x1_me == 1){tt <- h2(strong(withMathJax(paste0("$$P(", -Inf, " < X <", Inf, " ) = ", round(input$input_p_x1, 3), "$$"))))}
+                        if(input$input_p_x1_me != 1){tt <- h2(strong(withMathJax(paste0("$$P( X <", round(prob_reac()[1], 3), ") = ", round(input$input_p_x1_me, 3), "$$"))))}
+                    }
+        tt
     })
     output$Normal_plot <- renderPlot({
         mean = input$mean_input; sd = input$sd_input; x0 = (mean-4*sd); x1 = (mean+4*sd);
-        if(input$tipo_input == 'Entre'){fd <- function(x){y <- dnorm(x, mean = input$mean_input, sd = input$sd_input); y[x < input$input_x1 | x > input$input_x2] <- NA; return(y)}} else
-        if(input$tipo_input == 'Maior que'){fd <- function(x){y <- dnorm(x, mean = input$mean_input, sd = input$sd_input); y[x < input$input_x1_ma] <- NA; return(y)}} else
-        if(input$tipo_input == 'Menor que'){fd <- function(x){y <- dnorm(x, mean = input$mean_input, sd = input$sd_input); y[x > input$input_x1_me] <- NA; return(y)}}
+        if(input$v_p_input == 'Por valores' & input$tipo_input == 'Entre'){fd <- function(x){y <- dnorm(x, mean = input$mean_input, sd = input$sd_input); y[x < input$input_x1 | x > input$input_x2] <- NA; return(y)}} else
+        if(input$v_p_input == 'Por valores' & input$tipo_input == 'Maior que'){fd <- function(x){y <- dnorm(x, mean = input$mean_input, sd = input$sd_input); y[x < input$input_x1_ma] <- NA; return(y)}} else
+        if(input$v_p_input == 'Por valores' & input$tipo_input == 'Menor que'){fd <- function(x){y <- dnorm(x, mean = input$mean_input, sd = input$sd_input); y[x > input$input_x1_me] <- NA; return(y)}}
+        if(input$v_p_input != 'Por valores' & input$tipo_p_input == 'Entre'){fd <- function(x){y <- dnorm(x, mean = input$mean_input, sd = input$sd_input); y[x < prob_reac()[1] | x > prob_reac()[2]] <- NA; return(y)}} else
+            if(input$v_p_input != 'Por valores' & input$tipo_p_input == 'Maior que'){fd <- function(x){y <- dnorm(x, mean = input$mean_input, sd = input$sd_input); y[x < prob_reac()[1]] <- NA; return(y)}} else
+                if(input$v_p_input != 'Por valores' & input$tipo_p_input == 'Menor que'){fd <- function(x){y <- dnorm(x, mean = input$mean_input, sd = input$sd_input); y[x > prob_reac()[1]] <- NA; return(y)}} else
+                    if(input$v_p_input != 'Por valores' & input$input_p_x1 == 1){fd <- function(x){y <- dnorm(x, mean = input$mean_input, sd = input$sd_input); return(y)}}
         ggplot(data.frame(x = c(x0, x1)), aes(x = x)) +
             stat_function(fun = dnorm, args = list(input$mean_input, input$sd_input), size = 1, alpha = 0.8)+
             stat_function(fun=fd, geom="area", fill="blue", alpha=0.2)+
---
+++ teste/app.R
@@ -0,0 +1,83 @@
+library(shiny)
+ui <- fluidPage(
+    titlePanel("Probabilidade e Distribuição Normal"),
+    sidebarLayout(
+        column(2,
+               tabsetPanel(
+                   tabPanel(title = "Valores",
+                            numericInput(inputId = "mean_input_v", label = "Média", value = 0, step = 1),
+                            numericInput(inputId = "sd_input_v", label = "Desvio Padrão", value = 1, step = 1, min = 0.1),
+                            selectInput(inputId = "tipo_input_v", label = "Tipo de modelo", choices = c("Entre", "Maior que", "Menor que"), selected = "Entre"),
+                            conditionalPanel(
+                                condition = "input.tipo_input_v == 'Entre'",
+                                numericInput(inputId = "input_x1_v", label = "X1", value = -1, step = 1),
+                                numericInput(inputId = "input_x2_v", label = "X2", value = 1, step = 1)
+                            ),
+                            conditionalPanel(
+                                condition = "input.tipo_input_v == 'Maior que'",
+                                numericInput(inputId = "input_x1_v_ma", label = "X1", value = -1, step = 1)
+                            ),
+                            conditionalPanel(
+                                condition = "input.tipo_input_v == 'Menor que'",
+                                numericInput(inputId = "input_x1_v_me", label = "X1", value = 1, step = 1)
+                            )),
+                   tabPanel(title = "Percentagem",
+                            numericInput(inputId = "mean_input_p", label = "Médiaaaaaa", value = 0, step = 1),
+                            numericInput(inputId = "sd_input_p", label = "Desvio Padrãoaaaaaaaa", value = 1, step = 1, min = 0.1),
+                            selectInput(inputId = "tipo_input_p", label = "Tipo de modeloaaaaaa", choices = c("Entre", "Maior que", "Menor que"), selected = "Entre"),
+                            conditionalPanel(
+                                condition = "input.tipo_input_p == 'Entre'",
+                                numericInput(inputId = "input_x1_p", label = "X1", value = -1, step = 1),
+                                numericInput(inputId = "input_x2_p", label = "X2", value = 1, step = 1)
+                            ),
+                            conditionalPanel(
+                                condition = "input.tipo_input_p == 'Maior que'",
+                                numericInput(inputId = "input_x1_p_ma", label = "X1", value = -1, step = 1)
+                            ),
+                            conditionalPanel(
+                                condition = "input.tipo_input_p == 'Menor que'",
+                                numericInput(inputId = "input_x1_p_me", label = "X1", value = 1, step = 1)
+                            ))
+               )),
+        mainPanel(width = 10,
+                  plotOutput("Normal_plot"),
+                  uiOutput("Prob_func")
+        )
+    )
+)
+server <- function(input, output) {
+    library(tidyverse)
+    prob_reac <- reactive(
+        if(input$tipo_input_v == 'Entre'){(pnorm(q = input$input_x2_v, mean = input$mean_input_v, sd = input$sd_input_v) - pnorm(q = input$input_x1_v, mean = input$mean_input_v, sd = input$sd_input_v))} else
+            if(input$tipo_input_v == 'Maior que'){1-pnorm(q = input$input_x1_v_ma, mean = input$mean_input_v, sd = input$sd_input_v)} else
+                if(input$tipo_input_v == 'Menor que'){pnorm(q = input$input_x1_v_me, mean = input$mean_input_v, sd = input$sd_input_v)}    
+    )
+    output$Prob_func <- renderUI({
+        if(input$tipo_input_v == 'Entre'){h2(strong(withMathJax(paste0("$$P(", input$input_x1_v, " < X <", input$input_x2_v, ") = ", round(prob_reac(), 3), "$$"))))} else
+            if(input$tipo_input_v == 'Maior que'){h2(strong(withMathJax(paste0("$$P( X > ", input$input_x1_v_ma, ") = ", round(prob_reac(), 3), "$$"))))} else
+                if(input$tipo_input_v == 'Menor que'){h2(strong(withMathJax(paste0("$$P( X <", input$input_x1_v_me, ") = ", round(prob_reac(), 3), "$$"))))}
+    })
+    output$Normal_plot <- renderPlot({
+        mean = input$mean_input_v; sd = input$sd_input_v; x0 = (mean-4*sd); x1 = (mean+4*sd);
+        if(input$tipo_input_v == 'Entre'){fd <- function(x){y <- dnorm(x, mean = input$mean_input_v, sd = input$sd_input_v); y[x < input$input_x1_v | x > input$input_x2_v] <- NA; return(y)}} else
+            if(input$tipo_input_v == 'Maior que'){fd <- function(x){y <- dnorm(x, mean = input$mean_input_v, sd = input$sd_input_v); y[x < input$input_x1_v_ma] <- NA; return(y)}} else
+                if(input$tipo_input_v == 'Menor que'){fd <- function(x){y <- dnorm(x, mean = input$mean_input_v, sd = input$sd_input_v); y[x > input$input_x1_v_me] <- NA; return(y)}}
+        ggplot(data.frame(x = c(x0, x1)), aes(x = x)) +
+            stat_function(fun = dnorm, args = list(input$mean_input_v, input$sd_input_v), size = 1, alpha = 0.8)+
+            stat_function(fun=fd, geom="area", fill="blue", alpha=0.2)+
+            geom_hline(yintercept = 0, size = 1.1, alpha = 0.9)+
+            labs(title = "Probabilidade",
+                 x = "Média",
+                 #y = "",
+                 caption = "Aquarela Advanced Analytics")+
+            theme(legend.position = "none",
+                  panel.grid = element_blank(),
+                  axis.title.y = element_blank(),
+                  axis.text.y = element_blank(),
+                  axis.ticks.y = element_blank(),
+                  #panel.background = element_blank()
+                  axis.text.x = element_text(face = "bold", size = 12),
+                  plot.caption = element_text(size = 15, face = "bold"))
+    })
+}
+shinyApp(ui = ui, server = server)
